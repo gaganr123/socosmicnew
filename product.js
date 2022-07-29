359a1000a1cc0961626aa766d098f8fb434bb254
gaganr123 <rawatgagan143@gmail.com>
2022-07-29 08:20:31 +0545

    zoom effect

---
+++ product.js
@@ -0,0 +1,87 @@
+const btns = document.querySelectorAll(".btn");
+const storeProducts = document.querySelectorAll(".store-product");
+// const search = document.getElementById(search);
+
+for (i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const filter = e.target.dataset.filter;
+    console.log(filter);
+
+    storeProducts.forEach((product) => {
+      if (filter === "all") {
+        product.style.display = "block";
+      } else {
+        if (product.classList.contains(filter)) {
+          product.style.display = "block";
+        } else {
+          product.style.display = "none";
+        }
+      }
+    });
+  });
+}
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product-details h2");
+
+// A BETTER WAY TO FILTER THROUGH THE PRODUCTS
+search.addEventListener("keyup", filterProducts);
+
+function filterProducts(e) {
+  const text = e.target.value.toLowerCase();
+  // console.log(productName[0]);
+  productName.forEach(function (product) {
+    const item = product.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      product.parentElement.parentElement.style.display = "block";
+    } else {
+      product.parentElement.parentElement.style.display = "none";
+    }
+  });
+}
+
+// This code has been replaced by the function(filterProducts) above which does a better job
+
+// search.addEventListener("keyup", (e) => {
+//     e.preventDefault();
+//     const searchValue = search.value.toLowerCase().trim();
+//     // alert(search.value);
+
+//     for (i = 0; i < storeProducts.length; i++) {
+//         if (storeProducts[i].classList.contains(searchValue)) {
+//             storeProducts[i].style.display = 'block';
+//         } else if (searchValue == "") {
+//             storeProducts[i].style.display = 'block';
+//         } else {
+//             storeProducts[i].style.display = 'none';
+//         }
+
+//     //    if (searchValue == "") {
+//     //     storeProducts[i].style.display = 'block';
+//     //    }
+
+//     }
+
+// })
+
+
+
+
+
+
+
+
+
+
+/***********zoom pic ko*********/
+function zoo(e){
+  var zoomer = e.currentTarget;
+  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+  x = offsetX/zoomer.offsetWidth*200
+  y = offsetY/zoomer.offsetHeight*200
+  zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
